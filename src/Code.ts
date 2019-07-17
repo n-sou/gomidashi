d71a1cc5a4e9a1a51d19e1fbeacfaed98719b3c4
n-sou <r.a.m.iidx@gmail.com>
2019-07-17 22:55:32 +0900

    LINEに通知する機能追加

--- src/Code.ts
+++ src/Code.ts
@@ -1,84 +1,48 @@
-// Optional Types
-const isDone: boolean = false
-const height: number = 6
-const bob: string = 'bob'
-const list1: number[] = [1, 2, 3]
-const list2: number[] = [1, 2, 3]
-
-enum Color {
-  Red,
-  Green,
-  Blue
-}
-
-const c: Color = Color.Green
-let notSure: any = 4
-notSure = 'maybe a string instead'
-notSure = false // okay, definitely a boolean
-function showMessage(data: string): void {
-  // Void
-  Logger.log(data)
-}
-showMessage('hello')
-
-// Classes
-class Hamburger {
-  constructor() {
-    // This is the constructor.
+function getTrashKind(day: number): string {
+  let trashKind: string = ''
+  switch (day) {
+    case 1:
+    case 4:
+      trashKind = '可燃ごみ'
+      break
+    case 3:
+      trashKind = '紙・布・ダンボール'
+      break
+    case 5:
+      trashKind = '不燃ごみ・ビン・カン'
+      break
+    case 6:
+      trashKind = 'プラスチック'
+      break
   }
-  public listToppings() {
-    // This is a method.
+  return trashKind
+}
+
+function createMessage(): string {
+  const date: Date = new Date()
+  date.setDate(date.getDate() + 1)
+
+  const trashKind: string = getTrashKind(date.getDay())
+  if (trashKind === '') return
+
+  const message: string = `明日は${trashKind}を捨てる日だよ`
+  return message
+}
+
+function notice(): void {
+  const APITOKEN: string = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  const options: any = {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${APITOKEN}`
+    },
+    payload: {
+      message: createMessage()
+    }
   }
+  const response: GoogleAppsScript.URL_Fetch.UrlFetchApp = UrlFetchApp.fetch(
+    'https://notify-api.line.me/api/notify',
+    options
+  )
+  Logger.log(response)
 }
-
-// Template strings
-const name = 'Sam'
-const age = 42
-console.log(`hello my name is ${name}, and I am ${age} years old`)
-
-// Rest arguments
-const add = (a: number, b: number) => a + b
-const args = [3, 5]
-add(...args) // same as `add(args[0], args[1])`, or `add.apply(null, args)`
-
-// Spread operator (array)
-const cde = ['c', 'd', 'e']
-const scale = ['a', 'b', ...cde, 'f', 'g'] // ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-
-// Spread operator (map)
-const mapABC = { a: 5, b: 6, c: 3 }
-const mapABCD = { ...mapABC, d: 7 } // { a: 5, b: 6, c: 3, d: 7 }
-
-// Destructure map
-const jane = { firstName: 'Jane', lastName: 'Doe' }
-const john = { firstName: 'John', lastName: 'Doe', middleName: 'Smith' }
-function sayName({ firstName, lastName, middleName = 'N/A' }) {
-  console.log(`Hello ${firstName} ${middleName} ${lastName}`)
-}
-sayName(jane) // -> Hello Jane N/A Doe
-sayName(john) // -> Helo John Smith Doe
-
-// Export (The export keyword is ignored)
-export const pi = 3.141592
-
-// Google Apps Script Services
-const doc = DocumentApp.create('Hello, world!')
-doc
-  .getBody()
-  .appendParagraph('This document was created by Google Apps Script.')
-
-// Decorators
-function Override(label: string) {
-  return (target: any, key: string) => {
-    Object.defineProperty(target, key, {
-      configurable: false,
-      get: () => label
-    })
-  }
-}
-class Test {
-  @Override('test') // invokes Override, which returns the decorator
-  public name: string = 'pat'
-}
-const t = new Test()
-console.log(t.name) // 'test'
